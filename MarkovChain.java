a9ed6ec578a3159338e392bb1dc8307d94ab8f5f
Shea Polanskky <env.laser@gmail.com>
2014-09-29 10:33:34 -0600

    Switched to using optionals instead of taking in a blank value

--- MarkovChain.java
+++ MarkovChain.java
@@ -1,24 +1,23 @@
 import java.util.*;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MarkovChain<T> {
     private final int order;
-    private final T blankValue;
     private final HashMap<MarkovKey, ProbabilityMapping> markovTable =
             new HashMap<>();
 
-    public MarkovChain(int order, T blankValue) {
+    public MarkovChain(int order) {
         this.order = order;
-        this.blankValue = blankValue;
     }
 
     public void addItems(Iterator<T> iterator) {
         ItemsAdder adder = new ItemsAdder();
-        iterator.forEachRemaining(adder);
-        Collections.nCopies(order, blankValue).iterator()
+        new IteratorMapper<>(iterator, Optional::of).forEachRemaining(adder);
+        Collections.nCopies(order, Optional.<T>empty()).iterator()
                 .forEachRemaining(adder); //padding with blanks at the end
     }
 
@@ -35,10 +34,10 @@ public class MarkovChain<T> {
                 .collect(Collectors.toList());
     }
 
-    private class ItemsAdder implements Consumer<T> {
+    private class ItemsAdder implements Consumer<Optional<T>> {
         private MarkovKey key = new MarkovKey();
 
-        public void accept(T t) {
+        public void accept(Optional<T> t) {
             if (!markovTable.containsKey(key)) {
                 markovTable.put(key, new ProbabilityMapping());
             }
@@ -56,23 +55,24 @@ public class MarkovChain<T> {
         }
 
         public T get() {
-            T t;
+            Optional<T> t;
             do {
                 t = markovTable.get(key).getNextTRandomly(rand);
                 key = key.getNext(t);
             }
-            while (t.equals(blankValue)); //no need to return blanks
-            return t;
+            while (!t.isPresent()); //no need to return blanks
+            return t.get();
         }
     }
 
     private class ProbabilityMapping {
         //LinkedHashMap so that getNextStringByIndex has a deterministic output
-        private final LinkedHashMap<T, Integer> probabilityMap =
+        //use of Optional<T> for a well-defined non-value for all types
+        private final LinkedHashMap<Optional<T>, Integer> probabilityMap =
                 new LinkedHashMap<>();
         private int totalValues;
 
-        public void add(T t) {
+        public void add(Optional<T> t) {
             if (probabilityMap.containsKey(t)) {
                 probabilityMap.put(t, probabilityMap.get(t) + 1);
             }
@@ -87,11 +87,11 @@ public class MarkovChain<T> {
             return totalValues;
         }
 
-        public T getNextTByIndex(int index) { //0 based
+        public Optional<T> getNextTByIndex(int index) { //0 based
             if (index >= getNumberOfSamples()) {
                 throw new IllegalArgumentException();
             }
-            for (T key : probabilityMap.keySet()) {
+            for (Optional<T> key : probabilityMap.keySet()) {
                 index -= probabilityMap.get(key);
                 if (index < 0) {
                     return key;
@@ -101,11 +101,11 @@ public class MarkovChain<T> {
             //earlier bounds checking
         }
 
-        public T getNextTRandomly() {
+        public Optional<T> getNextTRandomly() {
             return getNextTRandomly(new Random());
         }
 
-        public T getNextTRandomly(Random rand) {
+        public Optional<T> getNextTRandomly(Random rand) {
             return getNextTByIndex(rand.nextInt(getNumberOfSamples()));
         }
 
@@ -115,29 +115,29 @@ public class MarkovChain<T> {
     }
 
     private class MarkovKey {
-        private final LinkedList<T> prefixes;
+        private final LinkedList<Optional<T>> prefixes;
 
         public MarkovKey() {
-            this(Collections.nCopies(order, blankValue));
+            this(Collections.nCopies(order, Optional.empty()));
         }
 
-        public MarkovKey(Collection<T> clone) {
+        public MarkovKey(Collection<Optional<T>> clone) {
             prefixes = new LinkedList<>(clone);
         }
 
-        public MarkovKey(Collection<T> previous, T next) {
+        public MarkovKey(Collection<Optional<T>> previous, Optional<T> next) {
             this(previous);
             prefixes.addLast(next);
             prefixes.removeFirst();
         }
 
-        public MarkovKey getNext(T next) {
+        public MarkovKey getNext(Optional<T> next) {
             return new MarkovKey(prefixes, next);
         }
 
         @Override
         @SuppressWarnings("unchecked") //I'm checking it but java doesn't
-        //think so, and I compile with -Werror so I need to suppress the warning
+        //think so and I compile with -Werror so I need to suppress the warning
         public boolean equals(Object o) {
             return o != null && o.getClass().equals(getClass()) &&
                     ((MarkovKey) o).prefixes.equals(prefixes);
@@ -151,6 +151,31 @@ public class MarkovChain<T> {
         @Override
         public String toString() {
             return prefixes.toString();
+        }
+    }
+
+    protected class IteratorMapper<T, R> implements Iterator<R> {
+        private final Iterator<T> wrapped;
+        private final Function<T, R> mapFunction;
+
+        public IteratorMapper(Iterator<T> wrapped, Function<T, R> mapFunction) {
+            this.wrapped = wrapped;
+            this.mapFunction = mapFunction;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return wrapped.hasNext();
+        }
+
+        @Override
+        public R next() {
+            return mapFunction.apply(wrapped.next());
+        }
+
+        @Override
+        public void remove() {
+            wrapped.remove();
         }
     }
 }
--- StringChain.java
+++ StringChain.java
@@ -3,43 +3,13 @@ import java.util.function.Function;
 
 public class StringChain extends MarkovChain<String> {
     public StringChain(int order) {
-        super(order, "");
+        super(order);
     }
-
-    /*@Override
-    public Stream<String> getRandomTStream(Random rand) {
-        return super.getRandomTStream(rand).map(s -> s + " ");
-    }*/
 
     @Override
     public void addItems(Iterator<String> iterator) {
         super.addItems(new IteratorMapper<>(iterator,
                 s -> s.length() > 1 ? s.trim() : s)); //trim spaces from words
         //but not from single characters
-    }
-
-    private class IteratorMapper<T, R> implements Iterator<R> {
-        private final Iterator<T> wrapped;
-        private final Function<T, R> mapFunction;
-
-        public IteratorMapper(Iterator<T> wrapped, Function<T, R> mapFunction) {
-            this.wrapped = wrapped;
-            this.mapFunction = mapFunction;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return wrapped.hasNext();
-        }
-
-        @Override
-        public R next() {
-            return mapFunction.apply(wrapped.next());
-        }
-
-        @Override
-        public void remove() {
-            wrapped.remove();
-        }
     }
 }
